2889851b7cac0d6796f4e20f6826710fd9e5a41c
meksoftwaredev <146220688+meksoftwaredev@users.noreply.github.com>
2024-02-25 19:38:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,24 +22,3 @@ function yapayzeka() {
     window.location.href = "https://copilot.microsoft.com/";
 }
 
-if (!('Notification' in window)) {
-    console.log('Bildirim özelliği bu tarayıcıda desteklenmiyor.');
-    return;
-}
-
-Notification.requestPermission().then((permission) => {
-    if (permission === 'granted') {
-      console.log('Bildirim izni alındı.');
-    } else {
-      console.log('Bildirim izni reddedildi.');
-    }
-});
-
-if (Notification.permission === 'granted') {
-    // Bildirim gösterebilirsiniz
-  } else if (Notification.permission === 'denied') {
-    // Kullanıcı bildirimi reddetmiş
-  } else {
-    // Kullanıcıya izin istemi gösterilebilir
-  }
-  
